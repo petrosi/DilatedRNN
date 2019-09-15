e4985f792435acef84f45a51ad6e878d4ced8172
Petros Iatropoulos <iatropoulos.petran@gmail.com>
2019-09-15 14:15:54 +0300

    Code for task 3 - language modelling

--- language_modelling.py
+++ language_modelling.py
@@ -1,104 +1,12 @@
-#############################################################################################
-
-### 4.3 Language modeling ###
-    
-### See at the beginning of the file !!! ###
-
-import collections
-import os
-import sys
-
 import tensorflow as tf
+import ptb_reader
+from models.dilated_rnn import DilatedRNN
+import numpy as np
+import math
+import time
+import pandas as pd
 
-Py3 = sys.version_info[0] == 3
-
-def _read_words(filename):
-  with tf.gfile.GFile(filename, "r") as f:
-    if Py3:
-      return f.read().replace("\n", "<eos>").split()
-    else:
-      return f.read().decode("utf-8").replace("\n", "<eos>").split()
-
-
-def _build_vocab(filename):
-  data = _read_words(filename)
-
-  counter = collections.Counter(data)
-  count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
-
-  words, _ = list(zip(*count_pairs))
-  word_to_id = dict(zip(words, range(len(words))))
-
-  return word_to_id
-
-
-def _file_to_word_ids(filename, word_to_id):
-  data = _read_words(filename)
-  return [word_to_id[word] for word in data if word in word_to_id]
-
-
-def ptb_raw_data(data_path=None):
-  """Load PTB raw data from data directory "data_path".
-  Reads PTB text files, converts strings to integer ids,
-  and performs mini-batching of the inputs.
-  The PTB dataset comes from Tomas Mikolov's webpage:
-  http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz
-  Args:
-    data_path: string path to the directory where simple-examples.tgz has
-      been extracted.
-  Returns:
-    tuple (train_data, valid_data, test_data, vocabulary)
-    where each of the data objects can be passed to PTBIterator.
-  """
-
-  train_path = os.path.join(data_path, "ptb.char.train.txt")
-  valid_path = os.path.join(data_path, "ptb.char.valid.txt")
-  test_path = os.path.join(data_path, "ptb.char.test.txt")
-
-  word_to_id = _build_vocab(train_path)
-  train_data = _file_to_word_ids(train_path, word_to_id)
-  valid_data = _file_to_word_ids(valid_path, word_to_id)
-  test_data = _file_to_word_ids(test_path, word_to_id)
-  vocabulary = len(word_to_id)
-  
-  return train_data, valid_data, test_data, vocabulary
-
-
-def ptb_producer(raw_data, batch_size, num_steps, name=None):
-  """Iterate on the raw PTB data.
-  This chunks up raw_data into batches of examples and returns Tensors that
-  are drawn from these batches.
-  Args:
-    raw_data: one of the raw data outputs from ptb_raw_data.
-    batch_size: int, the batch size.
-    num_steps: int, the number of unrolls.
-    name: the name of this operation (optional).
-  Returns:
-    A pair of Tensors, each shaped [batch_size, num_steps]. The second element
-    of the tuple is the same data time-shifted to the right by one.
-  Raises:
-    tf.errors.InvalidArgumentError: if batch_size or num_steps are too high.
-  """
-  with tf.name_scope(name, "PTBProducer", [raw_data, batch_size, num_steps]):
-    raw_data = tf.convert_to_tensor(raw_data, name="raw_data", dtype=tf.int32)
-
-    data_len = tf.size(raw_data)
-    batch_len = data_len // batch_size
-    data = tf.reshape(raw_data[0 : batch_size * batch_len], [batch_size, batch_len])
-
-    epoch_size = (batch_len - 1) // num_steps
-    assertion = tf.assert_positive(epoch_size, message="epoch_size == 0, decrease batch_size or num_steps")
-    with tf.control_dependencies([assertion]):epoch_size = tf.identity(epoch_size, name="epoch_size")
-
-    i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
-    x = tf.strided_slice(data, [0, i * num_steps],[batch_size, (i + 1) * num_steps])
-    x.set_shape([batch_size, num_steps])
-    y = tf.strided_slice(data, [0, i * num_steps + 1],[batch_size, (i + 1) * num_steps + 1])
-    y.set_shape([batch_size, num_steps])
-    
-    return x, y
-
-train_data, valid_data, test_data, vocab_size = ptb_raw_data("./PTB_dataset/")
+train_data, valid_data, test_data, vocab_size = ptb_reader.ptb_raw_data("./PTB_dataset/")
 
 tf.reset_default_graph()
 
@@ -107,7 +15,7 @@ unrolled_dim = 100
 num_of_layers = 5
 num_input = 1
 number_of_classes = vocab_size # number of different characters
-cell_type = "VanillaRNN"
+cell_type_list = ["VanillaRNN", "LSTM", "GRU"]
 hidden_units = 256 # hidden layer num of features
 dilations = [2**j for j in range(num_of_layers)]
 batch_size = 128
@@ -116,3 +24,132 @@ number_of_epochs= 4
 experiment = "PTB"
 decay = 0.9
 dropout = 0.1
+
+X_train, y_train = ptb_reader.ptb_producer(train_data, unrolled_dim, vocab_size)
+X_val, y_val = ptb_reader.ptb_producer(valid_data, unrolled_dim, vocab_size)
+X_test, y_test = ptb_reader.ptb_producer(train_data, unrolled_dim, vocab_size)
+
+X = tf.placeholder(dtype=tf.float32, shape=[None, X_train.shape[1], num_input])
+y = tf.placeholder(dtype=tf.float32, shape=[None, y_train.shape[1], number_of_classes])
+batch_size = tf.placeholder(tf.int64)
+
+# Create the dataset
+train_dataset = tf.data.Dataset \
+        .from_tensor_slices((X, y)) \
+        .shuffle(buffer_size=16*batch_size) \
+        .batch(batch_size, drop_remainder=True)
+
+validation_dataset = tf.data.Dataset \
+        .from_tensor_slices((X, y)) \
+        .batch(batch_size, drop_remainder=True)
+
+test_dataset = tf.data.Dataset \
+        .from_tensor_slices((X, y)) \
+        .batch(batch_size, drop_remainder=True)
+
+dataset_iterator = tf.data.Iterator.from_structure((tf.float32, tf.float32), ([None, 100, 1], [None, 100, 50]))
+batch_X, batch_y = dataset_iterator.get_next()
+
+train_iterator = dataset_iterator.make_initializer(train_dataset)
+val_iterator = dataset_iterator.make_initializer(validation_dataset)
+test_iterator = dataset_iterator.make_initializer(test_dataset)
+
+for cell_type in cell_type_list:
+
+    print("Starting new optimization process (" + experiment + ")")
+    print("Model: Dilated " + cell_type + " for PTB dataset")
+    
+    # Retrieve the predictions
+    pred_object = DilatedRNN(cell_type, hidden_units, dilations)
+    output_logits = pred_object.classification(batch_X, number_of_classes, experiment)
+
+    # Loss function
+    loss_func = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=output_logits, labels=batch_y))/tf.log(tf.constant(2.0))
+
+    # Optimizer
+    optimizer = tf.train.RMSPropOptimizer(l_rate, decay)
+    train = optimizer.minimize(loss_func)
+
+    # number of trainable params
+    t_params = np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()])
+    print('Number of trainable params= ' + '{:d}'.format(t_params))
+
+    # Initialize the variables (i.e. assign their default value)
+    init = tf.global_variables_initializer()
+    
+    # Add ops to save and restore all the variables.
+    saver = tf.train.Saver()
+
+
+    with tf.Session() as sess:
+        sess.run(init)
+
+        results_train_set = []
+        results_val_set = []
+
+        current_val_loss = math.inf
+        
+        start = time.time()
+        
+        for epoch in range(1, number_of_epochs+1):
+            
+            sess.run(train_iterator, feed_dict={ X: X_train, y: y_train, batch_size: 128 })
+
+            count_loss = 0; number_of_batches = 0
+
+            while(True):
+                try:
+                    # Run optimization
+                    batch_loss, _ = sess.run([loss_func, train])
+                    
+                    count_loss += batch_loss
+                    number_of_batches += 1
+                except tf.errors.OutOfRangeError:
+                    break
+        
+            train_loss = count_loss/number_of_batches
+            results_train_set.append((epoch, train_loss))
+            
+            sess.run(val_iterator, feed_dict={ X: X_val, y: y_val, batch_size: len(y_val) })
+            val_loss = sess.run(loss_func)
+            results_val_set.append((val_loss))
+    
+            # Check validation loss every 5 epochs
+            if epoch % 5 == 0 or epoch == 1:
+                # Early stopping and checkpointing
+                if val_loss > current_val_loss:
+                    saver.restore(sess, "PTB_Checkpoints/Dilated_" + cell_type + ".ckpt")
+                    break
+                else:
+                    current_val_loss = val_loss
+                    save_path = saver.save(sess, "PTB_Checkpoints/Dilated_" + cell_type + ".ckpt")
+                    print("Model saved in path: %s" % save_path)
+
+
+                print("Epoch " + str(epoch) + ", Training Loss= " + \
+                        "{:.4f}".format(train_loss) + ", Validation Loss= " + \
+                        "{:.4f}".format(val_loss))
+            
+        end = time.time()
+        
+        training_time = end - start
+        
+        print("Training Finished!")
+        
+        print("Training time for this model: ", training_time)
+        
+
+        
+        sess.run(test_iterator, feed_dict={ X: X_test, y: y_test, batch_size: len(y_test) })
+        test_loss = sess.run(loss_func)
+    
+        print("Testing BPC Loss=" + "{:.3f}".format(test_loss))
+
+    # Storing our results to a dataframe
+    results_train_set = pd.DataFrame(results_train_set)
+    results_val_set = pd.DataFrame(results_val_set)
+    
+    results = pd.concat([results_train_set, results_val_set], axis=1, join='outer', ignore_index=False)
+    results.columns = ["Epochs", "Training Loss", "Validation Loss"]
+    
+    export_csv = results.to_csv(r"./PTB_results/Dilated_" + cell_type + ".csv", index = None, header=True)
